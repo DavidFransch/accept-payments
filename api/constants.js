12558c081ca8009a38cf0ca122ffe437e62871c5
David <djsfdavid@gmail.com>
2020-11-26 09:55:50 +0200

    fix: test api key

--- api/constants.js
+++ api/constants.js
@@ -1,7 +1,7 @@
 // TODO: Move into .env
 const SECRET_TEST_API = {
   KEY:
-    "pk_test_51HbSZlHjgviKjH6jbzZHNDU9fVoLcaYNkFhkCuHBFjQ2uH9hZfwDFCGh2b1Iv45o4XS5MMEr7lPGQwbKyJvymZqN00vT91EWMI",
+    "sk_test_51HbSZlHjgviKjH6joKQXoRzbG9Zb4AUBdn14FUOnbTZM3lyKqRqtrU6nh8RDwXfVpgQZnHne1I8KYBgYtdcJ95jD00stleuqGf",
 }
 
 module.exports.SECRET_TEST_API = SECRET_TEST_API
